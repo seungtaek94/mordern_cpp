4cc0fc99948459d3969d7806025770040aac71ce
seungtaek94 <seungtaek.kim.94@gmail.com>
2022-09-22 08:50:54 +0900

    2장 빌더 업데이트 - 의도 알려주기 - 그루비 스타일 빌더

---
+++ 1_Creational_Patterns/2_Builder/2.4_indicate/indicate.cpp
@@ -0,0 +1,94 @@
+//
+// Created by admin on 2022-09-20.
+//
+#include <iostream>
+#include <sstream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <memory>
+
+using namespace std;
+struct HtmlBuilder;
+
+// 방법 2.
+struct HtmlElement
+{
+    friend HtmlBuilder;
+
+    string name;
+    string text;
+    vector<HtmlElement> elements;
+    const size_t indent_size = 2;
+
+    string str(int indent = 0) const
+    {
+        ostringstream oss;
+        string i(indent_size*indent, ' ');
+        oss << i << "<" << name << ">" << endl;
+        if (text.size() > 0)
+            oss << string(indent_size*(indent + 1), ' ') << text << endl;
+
+        for (const auto& e : elements)
+            oss << e.str(indent + 1);
+
+        oss << i << "</" << name << ">" << endl;
+        return oss.str();
+    }
+
+    static unique_ptr<HtmlBuilder> build(string root_name)
+    {
+        return make_unique<HtmlBuilder>(root_name);
+    }
+
+protected:
+    HtmlElement() {}
+    HtmlElement(const string& name, const string& text)
+            : name(name),
+              text(text)
+    {
+    }
+};
+
+
+struct HtmlBuilder
+{
+    HtmlBuilder(string root_name)
+    {
+        root.name = root_name;
+    }
+
+    // 공백 상태에서 시작한다.
+    HtmlBuilder& add_child(string child_name, string child_text)
+    {
+        HtmlElement e{ child_name, child_text };
+        root.elements.emplace_back(e);
+        return *this;
+    }
+
+    // 포인터에 기반한다.
+    HtmlBuilder* add_child_2(string child_name, string child_text)
+    {
+        HtmlElement e{ child_name, child_text };
+        root.elements.emplace_back(e);
+        return this;
+    }
+
+    string str() { return root.str(); }
+
+    operator HtmlElement() const { return root; }
+    HtmlElement root;
+};
+
+
+
+int main()
+{
+    HtmlElement e = HtmlElement::build("ul")
+            ->add_child("li", "hello")
+            .add_child("li", "world");
+
+    cout << e.str() << endl;
+
+    return 0;
+}
---
+++ 1_Creational_Patterns/2_Builder/2.5_groovy_style/groovy.cpp
@@ -0,0 +1,87 @@
+//
+// Created by admin on 2022-09-22.
+//
+#include <string>
+#include <vector>
+#include <iostream>
+
+struct Tag
+{
+    std::string name;
+    std::string text;
+    std::vector<Tag> children;
+    std::vector<std::pair<std::string, std::string>> attributes;
+
+    friend std::ostream& operator << (std::ostream& os, const Tag& tag)
+    {
+        os << "<" << tag.name;
+
+        for (const auto& att : tag.attributes)
+            os << " " << att.first << "=\"" << att.second << "\"";
+
+        if (tag.children.size() == 0 && tag.text.length() == 0)
+        {
+            os << "/>" << std::endl;
+        }
+        else
+        {
+            os << ">" << std::endl;
+
+            if (tag.text.length())
+                os << tag.text << std::endl;
+
+            for (const auto& child : tag.children)
+                os << child;
+
+            os << "</" << tag.name << ">" << std::endl;
+        }
+
+        return os;
+    }
+
+protected:
+    Tag(const std::string& name, const std::string& text)
+    :name(name), text(text)
+    {
+    }
+
+    Tag(const std::string& name, const std::vector<Tag>& children)
+    :name(name), children(children)
+    {
+    }
+};
+
+
+struct P : Tag
+{
+    explicit P(const std::string& text)
+    : Tag("p", text)
+    {
+    }
+
+    P(std::initializer_list<Tag> children)
+            : Tag("p", children)
+    {
+    }
+};
+
+struct IMG : Tag
+{
+    explicit IMG(const std::string& url)
+            : Tag{"img", ""}
+    {
+        attributes.emplace_back(make_pair("src", url));
+    }
+};
+
+int main()
+{
+    std::cout <<
+    P {
+        IMG {"http://pokemon.com/pikachu.png"},
+        IMG {"http://pokemon.com/pikachu.png"}
+    }
+    << std::endl;
+
+    return 0;
+}
